e3cf13e3e7b9afd4c19958459bc7a492297206ec
wonsyn <sde3715@naver.com>
2022-07-21 18:16:34 +0900

    frontend init

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,20 +1,6 @@
-import Vue from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
-
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+createApp(App).use(store).use(router).mount("#app");
