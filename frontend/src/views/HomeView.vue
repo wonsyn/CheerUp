0998bcfb379cd0b37809800e64cd0bc11c87b576
wonsyn <sde3715@naver.com>
2022-07-21 15:11:27 +0900

    frontend init

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -8,13 +8,8 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
