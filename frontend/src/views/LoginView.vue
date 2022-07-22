ed919c8095e4d55ccd634894bd920af598e86338
장진세 <holy_killer@naver.com>
2022-07-22 18:02:54 +0900

    S07P12B104-85 로그인 화면 입력칸 및 버튼 생성

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,9 +1,31 @@
 <template>
-  <div></div>
+  <div id="login">로그인</div>
+  <form>    
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="basic-addon1">아이디</span>
+      <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+    </div>
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="basic-addon2">비밀번호</span>
+      <input type="text" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon2" />
+    </div>
+    <button type="submit" class="btn btn-primary">로그인</button>
+  </form>
+  <div>
+    <p>social login</p>
+    <div><button>Sign in with Naver</button></div>
+    <div><button>Sign in with Kakao</button></div>
+    <div><button>Sign in with Facebook</button></div>
+    <div><button>Sign in with Twitter</button></div>
+    <div><button>Sign in with Google</button></div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "LoginView",
+  components: {},
+};
 </script>
 
 <style scoped></style>
