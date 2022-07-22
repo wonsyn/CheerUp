749e7f8e54bc3a1a63eaa2f863377830926b1a53
wonsyn <sde3715@naver.com>
2022-07-22 10:28:09 +0900

    라우터 링크 변경

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav v-if="isLogin == false">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/login">About</router-link>
+    <router-link to="/signup">SignUp</router-link>
   </nav>
   <router-view />
 </template>
