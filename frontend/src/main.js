9bf65b4f3c7142dc2fe61f955bdc6a1f1a7733b9
wonsyn <sde3715@naver.com>
2022-07-22 11:22:35 +0900

    bootstrap 5 세팅

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap";
+
 createApp(App).use(store).use(router).mount("#app");
