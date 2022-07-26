1095eafc300a7acb9b61da185b69a3afe054f962
장진세 <holy_killer@naver.com>
2022-07-27 04:11:22 +0900

    S07P12B104-195 네브바 유저 검색 자동완성 및 알림 드랍다운 UI 작업(임시)

--- frontend/src/components/layout/HeaderNavBar.vue
+++ frontend/src/components/layout/HeaderNavBar.vue
@@ -1,9 +1,143 @@
 <template>
-  <div></div>
+  <nav id="navbar-frame" class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/"><img src="../../assets/logo.png" alt="home" style="height: 50px" /></router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="#">메인</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="#">달력</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="#">일정</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="#">단어장</router-link>
+          </li>
+        </ul>
+        <div id="searchuser" class="nav-item dropdown">
+          <form @submit.prevent="autoFillInput(result[0])" class="dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+            <input type="text" v-model="userInput" @input="submitAutoComplete" placeholder="유저 이름" />
+          </form>
+          <ul v-if="!!userInput == true" id="user-search-box" class="dropdown-menu autocomplete disabled">
+            <li class="dropdown-item">결과</li>
+            <li class="dropdown-item" @click="searchUserAdd" style="cursor: pointer" v-for="(res, i) in result" :key="i">
+              <img src="../../assets/logo.png" alt="profile-img" style="height: 20px" />
+              <span>{{ res }}</span>
+            </li>
+          </ul>
+        </div>
+        <div class="nav-item dropdown">
+          <router-link class="nav-link dropdown-toggle dropdown-center" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">알림</router-link>
+          <ul class="dropdown-menu dropdown-menu-end">
+            <div v-for="day in dayBefore" :key="day">
+              <li>{{ day }}</li>
+              <li v-for="a in users" :key="a" class="d-flex">
+                <img src="../../assets/logo.png" alt="profile-img" style="height: 20px" />
+                <router-link class="dropdown-item" to="#">알림 - {{ a }}</router-link>
+              </li>
+              <li><hr class="dropdown-divider" /></li>
+            </div>
+          </ul>
+        </div>
+        <div class="nav-item"><router-link class="nav-link" to="#">Link</router-link></div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
-export default {};
+// import { mapGetters } from "vuex";
+
+export default {
+  name: "NavBar",
+  components: {},
+  data() {
+    return {
+      users: ["장진세", "장원석", "정제희", "윤원상", "연창모", "BongMyeong-dong", "DeokMyeong-Dong", "CheerUpChUp", "JangDae-Dong", "Ajax", "Algorithm", "Vuejs Vuex", "Java Spring"],
+      userInput: null,
+      result: [],
+      dayBefore: ["오늘", "어제", "2일 전", "3일 전", "4일 전", "5일 전", "6일 전", "1주 전", "2주 전", "3주 전"],
+    };
+  },
+  computed: {},
+  methods: {
+    // ...mapGetters(['isLoggedIn', 'currentUser']),
+    // username() {
+    //   return this.currentUser.username ? this.currentUser.username : 'guest'
+    // },
+    submitAutoComplete() {
+      const autocomplete = document.querySelector(".autocomplete");
+      if (this.userInput) {
+        autocomplete.classList.remove("disabled");
+        this.result = this.users.filter((user) => {
+          return user.match(new RegExp("^" + this.userInput, "i"));
+        });
+      } else {
+        this.result = [];
+        autocomplete.classList.add("disabled");
+      }
+    },
+    searchUserAdd(event) {
+      this.userInput = event.target.innerText;
+    },
+    autoFillInput() {
+      this.userInput = this.result[0];
+    },
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.navbar-main {
+  display: flex;
+  justify-content: space-between;
+}
+.navbar-element {
+  display: flex;
+  justify-content: space-around;
+  text-align: center;
+}
+form {
+  outline: none;
+  background-color: #00dd99;
+  border-radius: 15px;
+}
+input {
+  outline: none;
+  border-width: 0;
+  border-radius: 15px;
+  background: transparent;
+  padding-left: 1rem;
+}
+input::placeholder {
+  color: white;
+}
+input[type="text"] {
+  color: white;
+}
+#user-search-box {
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+}
+@media (max-width: 500px) {
+  .dropdown-menu {
+    height: 200px;
+    overflow-y: auto;
+  }
+}
+</style>
