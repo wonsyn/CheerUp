72942c36612f64e319dc086e380332a60dfdb4c9
DESKTOP-KVCQHCD\SSAFY <jangwonseok17@gmail.com>
2022-07-27 22:41:17 +0900

    jira 이슈 없음 뉴스 url로 뉴스에 해당하는 댓글들 조회 - 추후 DB 수정 후 뉴스의 고유 ID로 수정할 예정

--- backend/backend-java/src/main/java/com/web/curation/controller/CommentController.java
+++ backend/backend-java/src/main/java/com/web/curation/controller/CommentController.java
@@ -1,9 +1,12 @@
 package com.web.curation.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +23,13 @@ public class CommentController {
 	@Autowired
 	private CommentService commentService;
 	
+	// 아직 api가 정해재지 않아서 고유 주소를 인식하는 규칙에 따라서 url바귈수도 => commentUrl을 나중에는 고유 id로 바꿀 예정
+	@GetMapping("/read/{commentUrl}")
+	public ResponseEntity<List<CommentDto>> readCommentByUrl(@PathVariable String commentUrl){
+		System.out.println(commentService.readCommentByUrl(commentUrl));
+		return new ResponseEntity<List<CommentDto>>(commentService.readCommentByUrl(commentUrl), HttpStatus.OK);
+	}
+	
 	@PostMapping("/create")
 	public ResponseEntity<String> createComment(@RequestBody CommentDto commentDto) {
 		commentService.writeComment(commentDto);
@@ -34,4 +44,6 @@ public class CommentController {
 		
 		return new ResponseEntity<String>("success", HttpStatus.OK);
 	}
+	
+	
 }
