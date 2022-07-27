9b062be5c2fb6c7d6d3bf753ffa65242fe400795
장진세 <holy_killer@naver.com>
2022-07-27 11:28:43 +0900

    S07P12B104-205 footer 컴포넌트 생성 및 내부 구조 생성

---
+++ frontend/src/components/layout/MainFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-flex justify-content-around">
+    <div>
+      <img src="@/assets/logo.png" alt="logo" />
+    </div>
+    <div>
+      <div>팀장: 정제희</div>
+      <div>팀원: 윤원상</div>
+      <div>팀원: 장원석</div>
+      <div>팀원: 연창모</div>
+      <div>팀원: 장진세</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
