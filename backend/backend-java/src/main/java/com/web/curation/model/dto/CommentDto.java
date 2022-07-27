7fa29c0e9dd46896ea00396b5b6fb73dfca15156
DESKTOP-KVCQHCD\SSAFY <jangwonseok17@gmail.com>
2022-07-27 13:48:05 +0900

    S07P12B104-62 댓글 작성하기 기능 추가

--- backend/backend-java/src/main/java/com/web/curation/model/dto/CommentDto.java
+++ backend/backend-java/src/main/java/com/web/curation/model/dto/CommentDto.java
@@ -4,10 +4,49 @@ import lombok.Data;
 
 @Data
 public class CommentDto {
-
 	private int commentId;
 	private String commentUrl;
 	private String commentContent;
-	private int userId;
+	private String id;
 	private String commentDate;
+	public int getCommentId() {
+		return commentId;
+	}
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+	public String getCommentUrl() {
+		return commentUrl;
+	}
+	public void setCommentUrl(String commentUrl) {
+		this.commentUrl = commentUrl;
+	}
+	public String getCommentContent() {
+		return commentContent;
+	}
+	public void setCommentContent(String commentContent) {
+		this.commentContent = commentContent;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getCommentDate() {
+		return commentDate;
+	}
+	public void setCommentDate(String commentDate) {
+		this.commentDate = commentDate;
+	}
+	@Override
+	public String toString() {
+		return "CommentDto [commentId=" + commentId + ", commentUrl=" + commentUrl + ", commentContent="
+				+ commentContent + ", id=" + id + ", commentDate=" + commentDate + "]";
+	}
+	
+	
+	
+	
+
 }
--- backend/backend-java/src/main/java/com/web/curation/model/service/CommentServiceImpl.java
+++ backend/backend-java/src/main/java/com/web/curation/model/service/CommentServiceImpl.java
@@ -1,8 +1,19 @@
 package com.web.curation.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class CommentServiceImpl {
+import com.web.curation.model.dao.CommentDao;
+import com.web.curation.model.dto.CommentDto;
 
+@Service
+public class CommentServiceImpl implements CommentService{
+	
+	@Autowired
+	private CommentDao commentDao;
+
+	@Override
+	public void writeComment(CommentDto commentDto) {
+		commentDao.createComment(commentDto);
+	}
 }
