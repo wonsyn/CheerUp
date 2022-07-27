23a25fb08f6fbef5f5c6b89dbf7311a7d8f37c4c
장진세 <holy_killer@naver.com>
2022-07-27 17:00:09 +0900

    S07P12B104-210 유저 프로필 화면 구성 큰 틀만

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,9 +1,35 @@
 <template>
-  <div></div>
+  <div id="profile-view">
+    프로필
+    <div id="user-profile">
+      <div id="profile-img"></div>
+      <div id="user-info">
+        <div id="username"></div>
+        <div id="user-follow"></div>
+      </div>
+    </div>
+    <div id="user-scrap-and-board">
+      <div id="user-board-tab">
+        <user-board-list></user-board-list>
+      </div>
+      <div id="user-scrap-tab">
+        <user-scrap-list></user-scrap-list>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import UserBoardList from "@/components/UserBoardList.vue";
+import UserScrapList from "@/components/UserScrapList.vue";
+
+export default {
+  name: "ProfileView",
+  components: {
+    UserBoardList,
+    UserScrapList,
+  },
+};
 </script>
 
 <style scoped></style>
