311e5a982ab03572bdc4e0b928f9d77439493ea3
장진세 <holy_killer@naver.com>
2022-07-29 10:57:13 +0900

    S07P12B104-210 라우터 기본 작업

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,13 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/LoginView.vue";
 import SignUpView from "@/views/SignUpView.vue";
-// import CalendarView from "@/views/CalendarView.vue"
-// import FollowView from "@/views/FollowView.vue"
-// import NotFoundView from "@/views/NotFoundView.vue"
-// import ProfileView from "@/views/ProfileView.vue"
-// import ScheduleView from "@/views/ScheduleView.vue"
-// import UserEditView from "@/views/UserEditView.vue"
-// import VocaView from "@/views/VocaView.vue"
+import CalendarView from "@/views/CalendarView.vue";
+import FollowView from "@/views/FollowView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
+import ProfileView from "@/views/ProfileView.vue";
+import ScheduleView from "@/views/ScheduleView.vue";
+import UserEditView from "@/views/UserEditView.vue";
+import VocaView from "@/views/VocaView.vue";
 
 const routes = [
   {
@@ -25,6 +25,45 @@ const routes = [
     path: "/signup",
     name: "signup",
     component: SignUpView,
+  },
+  {
+    path: "/calendar",
+    name: "calendar",
+    component: CalendarView,
+  },
+  {
+    path: "/follow",
+    name: "follow",
+    component: FollowView,
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: ProfileView,
+  },
+  {
+    path: "/schedule",
+    name: "schedule",
+    component: ScheduleView,
+  },
+  {
+    path: "/edit",
+    name: "useredit",
+    component: UserEditView,
+  },
+  {
+    path: "/voca",
+    name: "voca",
+    component: VocaView,
+  },
+  {
+    path: "/404",
+    name: "notfound",
+    component: NotFoundView,
+  },
+  {
+    path: "*",
+    redirect: "/404",
   },
 ];
 
