89e79b852fd92e9af2457ee614da64dc013c99c2
장진세 <holy_killer@naver.com>
2022-07-31 20:58:33 +0900

    S07P12B104-215 스크랩리스트

--- frontend/src/components/UserScrapList.vue
+++ frontend/src/components/UserScrapList.vue
@@ -1,9 +1,31 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        col
+        <feed-list-item></feed-list-item>
+      </div>
+      <div class="col">
+        col
+        <feed-list-item></feed-list-item>
+      </div>
+      <div class="col">
+        col
+        <feed-list-item></feed-list-item>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import FeedListItem from "@/components/CommentListItem";
+
+export default {
+  name: UserScrapList,
+  components: {
+    FeedListItem,
+  },
+};
 </script>
 
 <style scoped></style>
