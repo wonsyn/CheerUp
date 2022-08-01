1c24dc67aa894db375f056e4947aa7902450dd2f
DESKTOP-KVCQHCD\SSAFY <jangwonseok17@gmail.com>
2022-08-01 13:19:21 +0900

    JIRA 이슈 없음 [Update] Swagger 추가

--- backend/backend-java/src/main/java/com/web/curation/controller/FeedController.java
+++ backend/backend-java/src/main/java/com/web/curation/controller/FeedController.java
@@ -22,6 +22,7 @@ import com.web.curation.model.service.FeedService;
 import com.web.curation.model.service.UserScrapfeedMyfeedService;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 
 @Api(tags = "메인피드/개인피드/보드")
@@ -38,18 +39,24 @@ public class FeedController {
 	@Autowired
 	private FeedService feedService;
 	
+	@ApiOperation(value="유저별 생성 board 불러오기", 
+			  notes="userId로 유저가 생성한 board들 불러오기")
 	@GetMapping("/board/list/{userId}")
 	public ResponseEntity<List<BoardDto>> getBoardByUserId(@PathVariable int userId){
 		
 		return new ResponseEntity<List<BoardDto>>(boardService.getBoardByUserId(userId), HttpStatus.OK);
 	}
 	
+	@ApiOperation(value="board 생성", 
+			  notes="유저 board 생성")
 	@PostMapping("/board/create")
 	public ResponseEntity<String> createBoard(@RequestBody BoardDto boardDto){
 		boardService.createBoard(boardDto);
 		return new ResponseEntity<String>("success", HttpStatus.OK);
 	}
 	
+	@ApiOperation(value="board 수정", 
+			  notes="유저 board 이름 수정")
 	@PutMapping("/board/update")
 	public ResponseEntity<String> editBoard(@RequestBody BoardDto boardDto){
 		System.out.println(boardDto);
@@ -57,12 +64,16 @@ public class FeedController {
 		return new ResponseEntity<String>("success", HttpStatus.OK);
 	}
 	
+	@ApiOperation(value="board 삭제", 
+			  notes="유저 board 삭제")
 	@DeleteMapping("/board/delete/{boardId}")
 	public ResponseEntity<String> deleteBoard(@PathVariable int boardId){
 		boardService.deleteBoard(boardId);
 		return new ResponseEntity<String>("success", HttpStatus.OK);
 	}
 	
+	@ApiOperation(value="스크랩하기", 
+			  notes="피드 스크랩하기")
 	@PostMapping("/scrap/create")
 	public ResponseEntity<String> createScrap(@RequestBody UserScrapfeedMyfeedDto dto){
 		System.out.println(dto);
@@ -70,17 +81,23 @@ public class FeedController {
 		return new ResponseEntity<String>("success", HttpStatus.OK);
 	}
 	
+	@ApiOperation(value="내가 스크랩 한 피드 보기", 
+			  notes="userId로 내가 스크랩 한 피드 모두 보기")
 	@GetMapping("/scrap/{userId}")
 	public ResponseEntity<List<UserScrapfeedMyfeedDto>> getMyFeed(@PathVariable int userId){
 		return new ResponseEntity<List<UserScrapfeedMyfeedDto>>(userScrapfeedMyfeedService.getMyScrap(userId), HttpStatus.OK);
 	}
 	
+	@ApiOperation(value="보드 별 내가 스크랩 한 피드 보기", 
+			  notes="userId와 boardId로 내가 스크랩 한 피드 보기")
 	@GetMapping("/scrap/{userId}/{boardId}")
 	public ResponseEntity<List<UserScrapfeedMyfeedDto>> getMyFeedEachBoard(@PathVariable int userId, @PathVariable int boardId){
 		System.out.println(userId);
 		return new ResponseEntity<List<UserScrapfeedMyfeedDto>>(userScrapfeedMyfeedService.getMyScrapEachBoard(userId, boardId), HttpStatus.OK);
 	}
 	
+	@ApiOperation(value="피드 타입 별로 조회", 
+			  notes="main 피드의 타입(뉴스, 정보글 등)별로 피드 정렬")
 	@GetMapping("/{feedType}")
 	public ResponseEntity<List<FeedDto>> searchFeed(@PathVariable int feedType){
 		System.out.println(feedType);
