a78b20801c31294812b3bf27ca9eebe4e201edbd
ruderp <ruderp@naver.com>
2022-08-02 15:06:37 +0900

    S07P12B104-66 [ADD] 알람 생성하기 기능 추가

--- backend/backend-java/src/main/java/com/web/curation/model/dao/AlarmDao.java
+++ backend/backend-java/src/main/java/com/web/curation/model/dao/AlarmDao.java
@@ -1,5 +1,10 @@
 package com.web.curation.model.dao;
 
+import java.sql.SQLException;
+
+import com.web.curation.model.dto.AlarmDto;
+
 public interface AlarmDao {
 
+	public int createAlarm(AlarmDto alarmDto) throws SQLException;
 }
