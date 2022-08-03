e8e7b3572960e4ce46b7bebaab60c0dcfb67e9a0
wonsyn <sde3715@naver.com>
2022-08-03 13:35:06 +0900

    S07P12B104-269 [Add] Login store 구현

--- frontend/src/store/modules/User.js
+++ frontend/src/store/modules/User.js
@@ -1,9 +1,43 @@
-import { createStore } from "vuex";
+import { login } from "@/api/Feature/User.js";
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+const state = {
+  isLogin: false,
+};
+
+const getters = {};
+
+const mutations = {
+  SET_IS_LOGIN: (isLogin) => {
+    state.isLogin = isLogin;
+  },
+};
+
+const actions = {
+  async login(user) {
+    await login(
+      user,
+      ({ data }) => {
+        console.log(data);
+        if (data["message"] === "success") {
+          let access_token = data["access-token"];
+          let refresh_token = data["refresh-token"];
+          mutations.SET_IS_LOGIN(true);
+          sessionStorage.setItem("access-token", access_token);
+          sessionStorage.setItem("refresh_token", refresh_token);
+        } else {
+          mutations.SET_IS_LOGIN(false);
+        }
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
+  },
+};
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions,
+};
