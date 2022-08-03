4e76f1fc6d2dbfc812d59c8df35c10a17a67fcbc
wonsyn <sde3715@naver.com>
2022-08-03 13:32:44 +0900

    Jira이슈 없음 [Add] main.js Vuex 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "@/router";
 import store from "./store";
+import Vuex from "vuex";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).use(Vuex).mount("#app");
