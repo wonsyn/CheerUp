c769776650c8e8606ad591676be41dfd21be5308
wonsyn <sde3715@naver.com>
2022-08-05 11:03:51 +0900

    Jira이슈 없음 [Refactor] 토큰 위치 수정

--- frontend/src/api/Feature/Feed.js
+++ frontend/src/api/Feature/Feed.js
@@ -1,16 +1,16 @@
 import { apiInstance } from "../index.js";
 
 const api = apiInstance();
+const access_token = sessionStorage.getItem("access-token");
 
 async function getFeed(params, success, fail) {
-  const access_token = sessionStorage.getItem("access-token");
   await api
     .get(`/feed/main`, {
       params: {
         industry: params.industry,
         type: params.type,
       },
-      headers: { Authorization: access_token },
+      headers: { "access-token": access_token },
     })
     .then(success)
     .catch(fail);
