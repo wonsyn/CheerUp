5dd979bf0c33d2d1fc0c5075f321df6c6dfb183a
wonsyn <sde3715@naver.com>
2022-08-08 09:21:10 +0900

    S07P12B104-290 [Add] 단어장 grid 설정

--- frontend/src/views/VocaView.vue
+++ frontend/src/views/VocaView.vue
@@ -1,9 +1,47 @@
 <template>
-  <div></div>
+  <div>
+    <div class="container">
+      <div class="d-flex">
+        <div class="me-auto"></div>
+        <form class="me-1" @submit.prevent="searchVoca" style="width: 30%">
+          <input type="text" v-model="searchWord" placeholder="검색할 단어를 입력하세요." style="width: 100%; border: 0px solid black; border-bottom: 1px solid black" />
+        </form>
+        <img src="@/assets/magnifying-glass.png" alt="search" style="width: 2%; height: 2%" />
+      </div>
+      <div class="row">
+        <div class="col">
+          col
+          <voca-list-item></voca-list-item>
+        </div>
+        <div class="col">
+          col
+          <voca-list-item></voca-list-item>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import VocaListItem from "@/components/VocaListItem.vue";
+
+export default {
+  component: {
+    VocaListItem,
+  },
+  data() {
+    return {
+      searchWord: null,
+    };
+  },
+  methods: {
+    searchVoca() {},
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+</style>
