85ca6379b6669d62584c465bd524cac7de2d8f28
wonsyn <sde3715@naver.com>
2022-08-08 14:50:36 +0900

    S07P12B104-290 [Add] 단어장 목록 구현

--- frontend/src/components/VocaListItem.vue
+++ frontend/src/components/VocaListItem.vue
@@ -1,11 +1,44 @@
 <template>
-  <div></div>
+  <div class="py-3 px-5 my-3" style="background-color: rgb(245, 245, 245); border-radius: 10px">
+    <div class="d-flex">
+      <span class="ps-2" style="font-weight: bold; font-size: 20px">{{ personalWord }}</span>
+    </div>
+    <hr />
+    <div style="text-align: left; font-size: 14px">{{ personalWordExp }}</div>
+    <br />
+    <div class="d-flex">
+      <div class="me-auto">{{ personalWordDate }}</div>
+      <img class="me-2" src="@/assets/edit.png" alt="edit" style="width: 4%; height: 4%" />
+      <img v-if="isFav" class="me-2" src="@/assets/star_filled.png" alt="fav" style="width: 4%; height: 4%" />
+      <img v-else class="me-2" src="@/assets/star_outline.png" alt="fav" style="width: 4%; height: 4%" />
+      <img src="@/assets/delete.png" alt="delete" style="width: 4%; height: 4%" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  props: {
+    personalWordId: Number,
+    userId: Number,
+    personalWord: String,
+    personalWordExp: String,
+    personalFavWord: Number,
+    personalWordDate: String,
+  },
+  date() {
+    return {
+      isFav: false,
+    };
+  },
   methods: {},
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+hr {
+  background-color: #00dd99;
+  height: 3px;
+  border: 0px;
+}
+</style>
--- frontend/src/views/VocaView.vue
+++ frontend/src/views/VocaView.vue
@@ -1,22 +1,18 @@
 <template>
-  <div>
-    <div class="container">
-      <div class="d-flex">
-        <div class="me-auto mt-3"></div>
-        <form class="me-1" @submit.prevent="searchVoca" style="width: 30%">
-          <input type="text" v-model="searchWord" placeholder="검색할 단어를 입력하세요." style="width: 100%; border: 0px solid black; border-bottom: 1px solid black" />
-        </form>
-        <img src="@/assets/magnifying-glass.png" alt="search" style="width: 2%; height: 2%" />
+  <div class="container">
+    <div class="d-flex">
+      <div class="me-auto mt-3"></div>
+      <form class="me-1" @submit.prevent="searchVoca" style="width: 30%">
+        <input type="text" v-model="searchWord" placeholder="검색할 단어를 입력하세요." style="width: 100%; border: 0px solid black; border-bottom: 1px solid black" />
+      </form>
+      <img src="@/assets/magnifying-glass.png" alt="search" style="width: 2%; height: 2%" />
+    </div>
+    <div class="row py-3">
+      <div class="col">
+        <voca-list-item v-for="voca in list1" :key="voca.personalWordId" v-bind="voca"></voca-list-item>
       </div>
-      <div class="row">
-        <div class="col">
-          col
-          <voca-list-item></voca-list-item>
-        </div>
-        <div class="col">
-          col
-          <voca-list-item></voca-list-item>
-        </div>
+      <div class="col">
+        <voca-list-item v-for="voca in list2" :key="voca.personalWordId" v-bind="voca"></voca-list-item>
       </div>
     </div>
   </div>
@@ -24,15 +20,33 @@
 
 <script>
 import VocaListItem from "@/components/VocaListItem.vue";
+import useStore from "@/store/index.js";
+
+const wordStore = useStore().modules.wordStore;
 
 export default {
-  component: {
+  components: {
     VocaListItem,
   },
   data() {
     return {
       searchWord: null,
+      myVocaList: [],
+      list1: [],
+      list2: [],
     };
+  },
+  async created() {
+    await wordStore.actions.getMyWordList(1);
+    this.myVocaList = wordStore.getters.getMyWordList();
+
+    for (let i = 0; i < this.myVocaList.length; i++) {
+      if (i % 2 == 0) {
+        this.list1.push(this.myVocaList[i]);
+      } else {
+        this.list2.push(this.myVocaList[i]);
+      }
+    }
   },
   methods: {
     searchVoca() {},
