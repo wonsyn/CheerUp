24d4c135716db00fb909f734af5d75a520e54072
wonsyn <sde3715@naver.com>
2022-08-11 14:09:49 +0900

    S07P12B104-367 [Add] 일정 목록 구현 (미완)

--- frontend/src/views/ScheduleView.vue
+++ frontend/src/views/ScheduleView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div style="color: #00dd99; font-size: 30px; font-weight: bold">내 일정</div>
+    <div class="d-flex">
+      <div class="me-auto"></div>
+      <button class="btn" style="background-color: lightseagreen; color: white">일정 추가하기</button>
+    </div>
+  </div>
 </template>
 
 <script>
