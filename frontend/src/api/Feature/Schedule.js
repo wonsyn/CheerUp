adbc6cc9cbe4d308af9f8e22fb5fcb1e93a8bc8c
wonsyn <sde3715@naver.com>
2022-08-14 15:56:34 +0900

    Jira 이슈 없음 [Fix] access-token 관련 에러 수정

--- frontend/src/api/Feature/Schedule.js
+++ frontend/src/api/Feature/Schedule.js
@@ -1,12 +1,33 @@
 import { apiInstance } from "@/api/index.js";
 
 const api = apiInstance();
-const headers = {
-  "access-token": sessionStorage.getItem("access-token"),
-};
 
 async function getSchedule(params, success, fail) {
-  await api.get(`/schedule/list`, { params: params, headers: headers }).then(success).catch(fail);
+  await api
+    .get(`/schedule/list`, { params: params, headers: { "access-token": sessionStorage.getItem("access-token") } })
+    .then(success)
+    .catch(fail);
 }
 
-export { getSchedule };
+async function addSchedule(body, success, fail) {
+  await api
+    .post(`/schedule/create`, JSON.stringify(body), { headers: { "access-token": sessionStorage.getItem("access-token") } })
+    .then(success)
+    .catch(fail);
+}
+
+async function updateSchedule(body, success, fail) {
+  await api
+    .put(`/schedule/update/`, JSON.stringify(body), { headers: { "access-token": sessionStorage.getItem("access-token") } })
+    .then(success)
+    .catch(fail);
+}
+
+async function deleteSchedule(scheduleId, success, fail) {
+  await api
+    .delete(`/schedule/delete/${scheduleId}`, { headers: { "access-token": sessionStorage.getItem("access-token") } })
+    .then(success)
+    .catch(fail);
+}
+
+export { getSchedule, addSchedule, updateSchedule, deleteSchedule };
