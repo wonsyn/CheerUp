591d1b30a3dd30bcccfb67f5c483d5d795494df2
wonsyn <sde3715@naver.com>
2022-08-16 13:37:48 +0900

    Jira이슈 없음 [Update] 메인 피드 사이즈 제한 추가, grid수정

--- frontend/src/components/FeedList.vue
+++ frontend/src/components/FeedList.vue
@@ -1,17 +1,9 @@
 <template>
   <div class="container">
-    <div class="row">
-      <!-- <div class="col-4" v-for="feed in list" :key="feed.feedId" v-bind="feed">
-        <feed-list-item :feed="feed"></feed-list-item>
-      </div> -->
-      <div class="col">
-        <feed-list-item v-for="feed in list1" :key="feed.feedId" v-bind="feed"></feed-list-item>
-      </div>
-      <div class="col">
-        <feed-list-item v-for="feed in list2" :key="feed.feedId" v-bind="feed"></feed-list-item>
-      </div>
-      <div class="col">
-        <feed-list-item v-for="feed in list3" :key="feed.feedId" v-bind="feed"></feed-list-item>
+    <div class="row d-flex justify-content-center">
+      <feed-list-item class="col-auto" v-for="feed in list" :key="feed.feedId" v-bind="feed"></feed-list-item>
+      <div class="px-3">
+        <button class="btn btn-primary" style="width: 100%" @click="moreFeed">더보기</button>
       </div>
     </div>
   </div>
@@ -30,56 +22,46 @@ export default {
   },
   data() {
     return {
-      list1: [],
-      list2: [],
-      list3: [],
       list: [],
       size: 20,
     };
   },
+  mounted() {},
   created() {
+    for (let i = 0; i < this.size; i++) {
+      this.list.push(this.feedList[i]);
+      if (i == this.feedList.length - 1) {
+        break;
+      }
+    }
+    console.log(this.list);
     console.log("FeedList.vue created start");
     console.log(this.feedList);
 
-    // for (let i = 0; i < this.size; i++) {
-    //   this.list.push(this.feedList[i]);
-    // }
-
-    for (let i = 0; i < this.feedList.length; i++) {
-      if (i % 3 == 0) {
-        this.list1.push(this.feedList[i]);
-      } else if (i % 3 == 1) {
-        this.list2.push(this.feedList[i]);
-      } else if (i % 3 == 2) {
-        this.list3.push(this.feedList[i]);
-      }
-      console.log(this.feedList[i]);
-    }
     console.log("FeedList.vue created end");
   },
   watch: {
     feedList() {
       console.log("FeedList.vue watch start");
 
-      // this.list.length = 0;
-      // for (let i = 0; i < this.size; i++) {
-      //   this.list.push(this.feedList[i]);
-      // }
-
-      this.list1.length = 0;
-      this.list2.length = 0;
-      this.list3.length = 0;
-      for (let i = 0; i < this.feedList.length; i++) {
-        if (i % 3 == 0) {
-          this.list1.push(this.feedList[i]);
-        } else if (i % 3 == 1) {
-          this.list2.push(this.feedList[i]);
-        } else if (i % 3 == 2) {
-          this.list3.push(this.feedList[i]);
-        }
-        console.log(this.feedList[i]);
+      this.list.length = 0;
+      for (let i = 0; i < this.size; i++) {
+        this.list.push(this.feedList[i]);
       }
+
       console.log("FeedList.vue watch end");
+    },
+  },
+  methods: {
+    moreFeed() {
+      let oldsize = this.size;
+      this.size = this.size + 20;
+      for (let i = oldsize; i < this.size; i++) {
+        this.list.push(this.feedList[i]);
+        if (i == this.feedList.length - 1) {
+          break;
+        }
+      }
     },
   },
 };
